84abc23ec9d463b8206e8e41c21c253fa05004dc
Richard Case <198425+richardcase@users.noreply.github.com>
2020-02-06 14:25:13 +0000

    fix: package/egg name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="k8s-dr-utils",
+    name="k8sdrutils",
     version="0.0.01",
     author="WW",
     description="Utils related to backup & restore",
