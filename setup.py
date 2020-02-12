b41c4e09e337e71e64b2928f7fb2c71fcbfeb723
Richard Case <198425+richardcase@users.noreply.github.com>
2020-02-12 12:19:16 +0000

    chore: bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="k8sdrutils",
-    version="0.0.01",
+    version="0.0.2",
     author="WW",
     description="Utils related to backup & restore",
     long_description=long_description,
