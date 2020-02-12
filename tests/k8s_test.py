1799d07cff49ce910189900f92067a362fa5239f
Richard Case <198425+richardcase@users.noreply.github.com>
2020-02-12 12:16:48 +0000

    fix: issue with get config test

--- tests/k8s_test.py
+++ tests/k8s_test.py
@@ -46,7 +46,7 @@ def test_read_kind_cm(mocker, datadir):
     assert result.metadata.namespace == "kube-system"
     
 def test_get_cluster_info(mocker, datadir):
-    k8s = K8s(cluster_name='cluster2')
+    k8s = create_k8s(datadir)
 
     patched = mocker.patch("kubernetes.client.apis.core_v1_api.CoreV1Api.read_namespaced_config_map", autospec=True)
     patched.return_value = create_response_data(datadir.join('clusterdata.json').strpath, 'V1ConfigMap')
